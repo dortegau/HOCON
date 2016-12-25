de98d1b5f03a002385adb18b43f939ba7f0338c1
Daniel Ortega <danielortegaufano@gmail.com>
2016-12-25 22:53:20 +0100

    first commit

---
+++ spec/hocon-spec.js
@@ -0,0 +1,73 @@
+var hoconParser = require('../lib/hocon');
+var expect = require('expect.js');
+
+describe('hocon test suite', function() {
+  describe('JSON subset features', function() {
+    it('should be parsed using null value', function() {
+      var jsonSubset = '{ "foo" : null }';
+      expect(hoconParser().parse(jsonSubset)).to.be.ok();
+    });
+
+    it('should be parsed using empty string value', function() {
+      var jsonSubset = '{ "foo" : "" }';
+      expect(hoconParser().parse(jsonSubset)).to.be.ok();
+    });
+
+    it('should be parsed using non-empty string value', function() {
+      var jsonSubset = '{ "foo" : "bar" }';
+      expect(hoconParser().parse(jsonSubset)).to.be.ok();
+    });
+
+    it('should be parsed using positive number value', function() {
+      var jsonSubset = '{ "foo" : 12 }';
+      expect(hoconParser().parse(jsonSubset)).to.be.ok();
+    });
+
+    it('should be parsed using negative number value', function() {
+      var jsonSubset = '{ "foo" : -12 }';
+      expect(hoconParser().parse(jsonSubset)).to.be.ok();
+    });
+
+    it('should be parsed using true boolean value', function() {
+      var jsonSubset = '{ "foo" : true }';
+      expect(hoconParser().parse(jsonSubset)).to.be.ok();
+    });
+
+    it('should be parsed using false boolean value', function() {
+      var jsonSubset = '{ "foo" : false }';
+      expect(hoconParser().parse(jsonSubset)).to.be.ok();
+    });
+
+    it('should be parsed using empty object value', function() {
+      var jsonSubset = '{ "foo" : {  } }';
+      expect(hoconParser().parse(jsonSubset)).to.be.ok();
+    });
+
+    it('should be parsed using filled object value', function() {
+      var jsonSubset = '{ "foo" : { "bar" : "baz" } }';
+      expect(hoconParser().parse(jsonSubset)).to.be.ok();
+    });
+
+    it('should be parsed using array of strings', function() {
+      var jsonSubset = '{ "foo" : [ "bar" , "baz" ] }';
+      expect(hoconParser().parse(jsonSubset)).to.be.ok();
+    });
+  });
+
+  describe('HOCON specific features', function(){
+    it('should be parsed using = instead of : as pair separator', function() {
+      var conf = '{ "a" = "b" }';
+      expect(hoconParser().parse(conf)).to.be.ok();
+    });
+
+    it('should be parsed without separator if a key is followed by {', function() {
+      var conf = '{ "a" { } }';
+      expect(hoconParser().parse(conf)).to.be.ok();
+    });
+
+    it('should be parsed when root braces are omitted', function() {
+      var conf = '"a" { }';
+      expect(hoconParser().parse(conf)).to.be.ok();
+    });
+  });
+});
