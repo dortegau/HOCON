5aae88044ed41b2c7088dc205c50fd7e7bfd9d18
Daniel Ortega <danielortegaufano@gmail.com>
2017-01-08 19:09:59 +0100

    supporting unquoted strings

--- spec/config-parser-spec.js
+++ spec/config-parser-spec.js
@@ -1,74 +1,74 @@
-var hoconParser = require('../lib/hocon');
+var ConfigParser = require('../lib/config-parser');
 var expect = require('expect.js');
 
-describe('HOCON test suite', function() {
+describe('Config parser test suite', function() {
   describe('JSON subset features', function() {
     it('should be parsed using null value', function() {
       var jsonSubset = '{ "foo" : null }';
-      var config = hoconParser.parse(jsonSubset);
+      var config = ConfigParser.parseString(jsonSubset);
 
       expect(config.values).to.eql({ foo : null });
     });
 
     it('should be parsed using empty string value', function() {
       var jsonSubset = '{ "foo" : "" }';
-      var config = hoconParser.parse(jsonSubset);
+      var config = ConfigParser.parseString(jsonSubset);
 
       expect(config.values).to.eql({ foo : "" });
     });
 
     it('should be parsed using non-empty string value', function() {
       var jsonSubset = '{ "foo" : "bar" }';
-      var config = hoconParser.parse(jsonSubset);
+      var config = ConfigParser.parseString(jsonSubset);
 
       expect(config.values).to.eql({ foo : "bar" });
     });
 
     it('should be parsed using positive number value', function() {
       var jsonSubset = '{ "foo" : 12 }';
-      var config = hoconParser.parse(jsonSubset);
+      var config = ConfigParser.parseString(jsonSubset);
 
       expect(config.values).to.eql({ foo : 12 });
     });
 
     it('should be parsed using negative number value', function() {
       var jsonSubset = '{ "foo" : -12 }';
-      var config = hoconParser.parse(jsonSubset);
+      var config = ConfigParser.parseString(jsonSubset);
 
       expect(config.values).to.eql({ foo : -12 });
     });
 
     it('should be parsed using true boolean value', function() {
       var jsonSubset = '{ "foo" : true }';
-      var config = hoconParser.parse(jsonSubset);
+      var config = ConfigParser.parseString(jsonSubset);
 
       expect(config.values).to.eql({ foo : true });
     });
 
     it('should be parsed using false boolean value', function() {
       var jsonSubset = '{ "foo" : false }';
-      var config = hoconParser.parse(jsonSubset);
+      var config = ConfigParser.parseString(jsonSubset);
 
       expect(config.values).to.eql({ foo : false });
     });
 
     it('should be parsed using empty object value', function() {
       var jsonSubset = '{ "foo" : {  } }';
-      var config = hoconParser.parse(jsonSubset);
+      var config = ConfigParser.parseString(jsonSubset);
 
       expect(config.values).to.eql({ foo : { } });
     });
 
     it('should be parsed using filled object value', function() {
       var jsonSubset = '{ "foo" : { "bar" : "baz" } }';
-      var config = hoconParser.parse(jsonSubset);
+      var config = ConfigParser.parseString(jsonSubset);
 
       expect(config.values).to.eql({ foo : { bar : "baz" } });
     });
 
     it('should be parsed using array of strings', function() {
       var jsonSubset = '{ "foo" : [ "bar" , "baz" ] }';
-      var config = hoconParser.parse(jsonSubset);
+      var config = ConfigParser.parseString(jsonSubset);
 
       expect(config.values).to.eql({ foo : [ "bar" , "baz" ] });
     });
@@ -77,49 +77,49 @@ describe('HOCON test suite', function() {
   describe('HOCON specific features', function(){
     it('should be parsed using = instead of : as pair separator', function() {
       var hoconString = '{ "a" = "b" }';
-      var config = hoconParser.parse(hoconString);
+      var config = ConfigParser.parseString(hoconString);
 
       expect(config.values).to.eql({ a : "b"});
     });
 
     it('should be parsed without separator if a key is followed by {', function() {
       var hoconString = '{ "a" { } }';
-      var config = hoconParser.parse(hoconString);
+      var config = ConfigParser.parseString(hoconString);
 
       expect(config.values).to.eql({ a : { } });
     });
 
     it('should be parsed when comments (with forward slashes) are present', function() {
       var hoconString = '//stupid comment\n"a" { }';
-      var config = hoconParser.parse(hoconString);
+      var config = ConfigParser.parseString(hoconString);
 
       expect(config.values).to.eql({ a : { } });
     });
 
     it('should be parsed when forward slashes are present in quoted strings', function() {
       var hoconString = '{ "a" = "//stupid string" }';
-      var config = hoconParser.parse(hoconString);
+      var config = ConfigParser.parseString(hoconString);
 
       expect(config.values).to.eql({ a : "//stupid string" });
     });
 
     it('should be parsed when comments (with number sign) are present', function() {
       var hoconString = '#stupid comment\n"a" { }';
-      var config = hoconParser.parse(hoconString);
+      var config = ConfigParser.parseString(hoconString);
 
       expect(config.values).to.eql({ a : { } });
     });
 
     it('should be parsed when number sign are present in quoted strings', function() {
       var hoconString = '{ "a" = "#stupid string" }';
-      var config = hoconParser.parse(hoconString);
+      var config = ConfigParser.parseString(hoconString);
 
       expect(config.values).to.eql({ a : "#stupid string" });
     });
 
     it('should be parsed when root braces are omitted', function() {
       var hoconString = '"a" { }';
-      var config = hoconParser.parse(hoconString);
+      var config = ConfigParser.parseString(hoconString);
 
       expect(config.values).to.eql({ a : { } });
     });
@@ -128,20 +128,20 @@ describe('HOCON test suite', function() {
       var hoconString = '"a" }';
 
       expect(function(){
-        hoconParser.parse(hoconString);
+        ConfigParser.parseString(hoconString);
       }).to.throwException(/Parse error/);
     });
 
     it('should be parsed when exists trailing comma in arrays', function() {
       var hoconString = '{ "foo" : [ "bar" , "baz" , ] }';
-      var config = hoconParser.parse(hoconString);
+      var config = ConfigParser.parseString(hoconString);
 
       expect(config.values).to.eql({ foo : [ "bar" , "baz" ] });
     });
 
     it('should be parsed when exists break lines instead of commas in arrays', function() {
       var hoconString = '{ "foo" : [ "bar" \n "baz" ] }';
-      var config = hoconParser.parse(hoconString);
+      var config = ConfigParser.parseString(hoconString);
 
       expect(config.values).to.eql({ foo : [ "bar" , "baz" ] });
     });
@@ -150,7 +150,7 @@ describe('HOCON test suite', function() {
       var hoconString = '{ "foo" : [ "bar" , "baz" , , ] }';
 
       expect(function() {
-        hoconParser.parse(hoconString);
+        ConfigParser.parseString(hoconString);
       }).to.throwException(/Parse error/);
     });
 
@@ -158,7 +158,7 @@ describe('HOCON test suite', function() {
       var hoconString = '{ "foo" : [ "bar" , , "baz" ] }';
 
       expect(function(){
-        hoconParser.parse(hoconString);
+        ConfigParser.parseString(hoconString);
       }).to.throwException(/Parse error/);
     });
 
@@ -166,13 +166,13 @@ describe('HOCON test suite', function() {
       var hoconString = '{ "foo" : [ , "bar" , "baz" ] }';
 
       expect(function(){
-        hoconParser.parse(hoconString);
+        ConfigParser.parseString(hoconString);
       }).to.throwException(/Parse error/);
     });
 
     it('should be parsed when exists trailing comma in objects', function() {
       var hoconString = '{ "foo" : { "bar" : "baz" , } }';
-      var config = hoconParser.parse(hoconString);
+      var config = ConfigParser.parseString(hoconString);
 
       expect(config.values).to.eql({ foo : { bar : "baz" } });
     });
@@ -181,7 +181,7 @@ describe('HOCON test suite', function() {
       var hoconString = '{ "foo" : { "bar" : "baz" , , } }';
 
       expect(function(){
-        hoconParser.parse(hoconString);
+        ConfigParser.parseString(hoconString);
       }).to.throwException(/Parse error/);
     });
 
@@ -189,7 +189,7 @@ describe('HOCON test suite', function() {
       var hoconString = '{ "foo" : { "bar" : "baz" , , "qux" : "quuz" } }';
 
       expect(function(){
-        hoconParser.parse(hoconString);
+        ConfigParser.parseString(hoconString);
       }).to.throwException(/Parse error/);
     });
 
@@ -197,14 +197,14 @@ describe('HOCON test suite', function() {
       var hoconString = '{ "foo" : { , "bar" : "baz" } }';
 
       expect(function(){
-        hoconParser.parse(hoconString);
+        ConfigParser.parseString(hoconString);
       }).to.throwException(/Parse error/);
     });
 
-    describe('duplicate keys and object merging', function(){
+    describe('Duplicate keys and object merging', function(){
       it('should be parsed when exists duplicate keys', function() {
         var hoconString = '{ "foo" : { "a" : "42" }, "foo" : { "b" : "43" } }';
-        var config = hoconParser.parse(hoconString);
+        var config = ConfigParser.parseString(hoconString);
         /*jshint -W075 */
         expect(config.values).to.eql({ foo : { a : "42" }, foo : { b : "43" } });
       });
@@ -213,25 +213,35 @@ describe('HOCON test suite', function() {
     describe('multiline strings', function(){
       it('should be parsed when exists multiline string with single line', function() {
         var hoconString = '{ "foo" : \'\'\'a\'\'\' }';
-        var config = hoconParser.parse(hoconString);
+        var config = ConfigParser.parseString(hoconString);
 
         expect(config.values).to.eql({ foo : "a" });
       });
 
       it('should be parsed when exists multiline string with multiple lines', function() {
         var hoconString = '{ "foo" : \'\'\'a \n a\'\'\' }';
-        var config = hoconParser.parse(hoconString);
+        var config = ConfigParser.parseString(hoconString);
 
         expect(config.values).to.eql({ foo : "a \n a" });
       });
 
       it('should be parsed when exists multiline string with single quote ending character', function() {
         var hoconString = '{ "foo" : \'\'\'a\'\'\'\' }';
-        var config = hoconParser.parse(hoconString);
+        var config = ConfigParser.parseString(hoconString);
 
         expect(config.values).to.eql({ foo : "a'" });
       });
     });
 */
+
   });
+
+  describe('Parse files test suite', function(){
+    it('should throw exception when given path is invalid', function(){
+      expect(function(){
+        ConfigParser.parseFile('./invalid_path');
+      }).to.throwException(/cannot read file/);
+    });
+  });
+
 });
